db024193dd2e538fcc5eec72256a5108e50646d0
Shane-Bath <>
2023-05-21 12:42:36 +0000

    Add Python queries

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -22,9 +22,12 @@ cursor = connection.cursor()
 # cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
 
 # Query 6 - Select all tracks where the composer is " Queen" fom the "Track" table
-#cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
 
-#
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Amy Winehouse"])
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [252])
+cursor.execute('SELECT * FROM "Track" WHERE "ArtistId" = %s', [51])
+
 
 # fetch the results (multiple)
 results = cursor.fetchall()
